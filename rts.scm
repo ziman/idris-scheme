55483fc1195d16d0aa79ff18d99ab38274a728b1
Matus Tejiscak <ziman@functor.sk>
2018-02-04 21:55:47 +0100

    More CFFI mocking.

--- rts.scm
+++ rts.scm
@@ -38,31 +38,37 @@
     (cons "this-program" (command-line-arguments))
     i))
 
+; Files are singleton list (mutable cells/boxes)
+; on EOF, we replace the contents with 'eof
 (define (cffi-fileOpen fname mode)
   (cond
     ((string=? mode "r")
-     (open-input-file fname))
+     (list (open-input-file fname)))
     ((string=? mode "w")
-     (open-output-file fname))
+     (list (open-output-file fname)))
     (else
       (error "unsupported open mode: " mode))))
 
 (define (cffi-isNull f)
-  0)  ; hackity-hack: if the file does not exist, it'll have crashed on fopen() already
+  (if (eq? (car f) 'null) 1 0))  ; hackity-hack: if the file does not exist, it'll have crashed on fopen() already
 
 (define (cffi-fileSize f)
-  (file-size f))
+  (file-size (car f)))
 
 (define (cffi-fileEOF f)
-  (if (eof-object? f) 1 0))
+  (if (eq? (car f) 'eof) 1 0))
 
 (define (cffi-readChars _world count f)
   (define (chars n)
     (if (= n 0) '()
-      (let ((char (read-char f)))
-        (if (eof-object? char) '()
+      (let ((char (read-char (car f))))
+        (if (eof-object? char)
+          (begin (set-car! f 'eof) '())
           (cons char (chars (- n 1)))))))
   (list->string (chars count)))
+
+(define (cffi-fileError f)
+  0) ; hackity-hack
 
 (define (cffi-idris_makeStringBuffer len)
   "")  ; hackity-hack
