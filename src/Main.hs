4ae4d075206d03db4451197064210f088a2f9c88
Matus Tejiscak <ziman@functor.sk>
2018-02-04 20:02:49 +0100

    Reformat Main.

--- src/Main.hs
+++ src/Main.hs
@@ -17,32 +17,33 @@ import System.Exit
 
 import Paths_idris_scheme
 
-data Opts = Opts { inputs :: [FilePath],
-                   output :: FilePath }
+data Opts = Opts
+    { inputs :: [FilePath]
+    , output :: FilePath
+    }
 
-showUsage = do putStrLn "Usage: idris-scheme <ibc-files> [-o <output-file>]"
-               exitWith ExitSuccess
+showUsage :: IO ()
+showUsage = do
+    putStrLn "Usage: idris-scheme <ibc-files> [-o <output-file>]"
+    exitWith ExitSuccess
 
 getOpts :: IO Opts
-getOpts = do xs <- getArgs
-             return $ process (Opts [] "a.scm") xs
+getOpts = process (Opts [] "a.scm") <$> getArgs
   where
     process opts ("-o":o:xs) = process (opts { output = o }) xs
-    process opts ("--yes-really":xs) = process opts xs -- TODO
     process opts (x:xs) = process (opts { inputs = x:inputs opts }) xs
     process opts [] = opts
 
-c_main :: Opts -> Idris ()
-c_main opts = do elabPrims
-                 loadInputs (inputs opts) Nothing
-                 mainProg <- elabMain
-                 ir <- compile (Via IBCFormat "scheme") (output opts) (Just mainProg)
-                 runIO $ codegenScheme ir
+cgMain :: Opts -> Idris ()
+cgMain opts = do
+    elabPrims
+    loadInputs (inputs opts) Nothing
+    mainProg <- elabMain
+    codegenInfo <- compile (Via IBCFormat "scheme") (output opts) (Just mainProg)
+    runIO $ codegenScheme codegenInfo
 
 main :: IO ()
-main = do opts <- getOpts
-          if (null (inputs opts)) 
-             then showUsage
-             else runMain (c_main opts)
-
-
+main =
+    getOpts >>= \case inputs opts of
+        [] -> showUsage
+        _  -> runMain (cgMain opts)
